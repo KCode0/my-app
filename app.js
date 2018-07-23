35e5de859138bdfd7b2f715134672cc69a0600f3
KCode0 <kyp5452@naver.com>
2018-07-23 17:45:00 +0900

    {commit-first commit};

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.send("Hello home page");
+});
+
+app.get('/route', function(req, res){
+    res.send('Hello Router, <img src="idontknow.png">');
+});
+
+app.get('/dynamic', function(req, res){
+
+    var lis = '';
+
+    for(var i=0; i<5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+
+    var time = Date();
+
+    var output = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Hello</title>
+        </head>
+        <body>
+            Hello, Dynamic!
+            <ul>
+                ${lis}
+            </ul>
+            ${time}
+        </body>
+    </html>`;
+    res.send(output);
+});
+
+
+app.get('/login', function(req, res){
+    res.send("Hello Login page");
+});
+
+app.listen(3000, function(){   
+    console.log("Conneted 3000");
+});
