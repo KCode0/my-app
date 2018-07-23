7491d80f098bad0ffda13186b8ac0876ec557cbc
KCode0 <kyp5452@naver.com>
2018-07-23 22:45:29 +0900

    20180723 22:44

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.locals.pretty = true;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.set('view engine', 'jade');
 app.set('views', './views');
-app.use(express.static('public'));
+
 
 app.get('/template', function(req, res){
-    res.render('temp');
+    res.render('temp', {time:Date(),_title:'안녕 타이틀이야'});
 });
 
 app.get('/', function(req,res){
@@ -48,6 +55,47 @@ app.get('/dynamic', function(req, res){
 app.get('/login', function(req, res){
     res.send("Hello Login page");
 });
+
+app.get('/topic/:id', function(req, res){
+    var topics = ['word0', 'word1', 'word2'];
+
+    var output = `
+        <a href="/topic?id=0">word0</a><br>
+        <a href="/topic?id=1">word1</a><br>
+        <a href="/topic?id=2">word2</a><br><br><br>
+        ${topics[req.params.id]}
+    `
+    res.send(output);
+
+});
+
+app.get('/topic/:id/:mode', function(req, res){
+    res.send(req.params.id + ' ::: ' + req.params.mode);
+});
+
+
+app.get('/form', function(req, res){
+    res.render('form');
+});
+
+app.post('/form_receiver', function(req, res){
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title + " ::: " + description);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, function(){   
     console.log("Conneted 3000");
